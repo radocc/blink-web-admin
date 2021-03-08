8f0486316173110f33966a4de4559d39103613e9
Anderson Almeida <anderson.almeida@radocc.com>
2021-03-08 07:29:57 -0300

    update

--- src/app/pages/configuracao/pages/usuario/usuario-cadastro/usuario-cadastro.component.ts
+++ src/app/pages/configuracao/pages/usuario/usuario-cadastro/usuario-cadastro.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms'; 
 import { PageCadastroComponent } from '@radocccomponentes/pagecadastro/pagecadastro.component';
+import { GrupoUsuario } from '@radoccmodels/base/grupousuario';
 import { Usuario } from '@radoccmodels/base/usuario';
 import { TipoConteudo } from '@radoccmodels/tipoconteudo';
+import { GrupoUsuarioService } from '@radoccservices/base/grupousuario-service';
 import { UsuarioService } from '@radoccservices/base/usuario-service';
 import { TipoConteudoService } from '@radoccservices/tipoconteudo-services';
 import { MessageService } from 'primeng/api';
@@ -12,7 +14,7 @@ import { MessageService } from 'primeng/api';
   templateUrl: './usuario-cadastro.component.html',
   styleUrls: ['./usuario-cadastro.component.scss'],
   providers:[ 
-    MessageService,UsuarioService
+    MessageService,UsuarioService,GrupoUsuarioService
   ]
 })
 export class UsuarioCadastroComponent implements OnInit {
@@ -29,26 +31,39 @@ export class UsuarioCadastroComponent implements OnInit {
   
   public form:FormGroup = new FormGroup({
     nome:new FormControl('', Validators.required),
-    sequencia:new FormControl('', Validators.required)
+    cnpj:new FormControl('', Validators.required),
+    login:new FormControl('', Validators.required),
+    email:new FormControl('', Validators.required),
+    senha:new FormControl('', Validators.required),
+    confirmeSenha:new FormControl('', Validators.required),
+    grupoUsuario:new FormControl('', Validators.required)
   }) 
   
   public usuario:Usuario;
+  public grupos:GrupoUsuario[] = [];
 
-  constructor(private msgService:MessageService, private usuarioService:UsuarioService) {
+  constructor(private msgService:MessageService, private usuarioService:UsuarioService,
+    private grupoUsuarioService:GrupoUsuarioService) {
 
   }
 
   ngOnInit(): void { 
   } 
   
+  public pesquisarGrupo(nome){
+    this.grupoUsuarioService.buscarPorNome(nome).subscribe((lista)=>{
+      this.grupos = lista;
+    })
+  }
+
   public salvar(event){    
     if (this.form.invalid){
       this.pageCadastro.showWarnMsg('EXISTEM_CAMPOS_INVALIDOS');
       return ;
     }
-    // if (this.tipo == null){
-    //   this.tipo = new TipoConteudo();
-    // }
+    if (this.usuario == null){
+      this.usuario = new Usuario();
+    }
     // this.tipo.nome = this.form.controls['nome'].value;
     // this.tipo.sequencia = this.form.controls['sequencia'].value;
     
---
+++ src/app/services/base/grupousuario-service.ts
@@ -0,0 +1,34 @@
+import { AbstractService } from './abstract-service'; 
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs'; 
+import { GrupoUsuario } from '@radoccmodels/base/grupousuario';
+
+@Injectable()
+export class GrupoUsuarioService extends AbstractService<GrupoUsuario> {
+
+    constructor(router: Router, http: HttpClient) {
+        super(router, http);
+    }
+
+    public getWebService(): string {
+        return 'grupousuario';
+    }
+
+    public findParaDireitosAcesso(): Observable<Array<GrupoUsuario>> {
+        return this.http.get<Array<GrupoUsuario>>(this.urlWebBase + "/findparadireitosacesso");
+    }
+
+    // public montarDireitoPadrao(idGrupo:number): Observable<GrupoResult>{
+    //     return this.http.get<GrupoResult>(this.urlWebBase + '/montardireitopadrao/'+idGrupo);
+    // }
+
+    public buscarPorNome(nome:string): Observable<Array<GrupoUsuario>> {
+        return this.http.post<Array<GrupoUsuario>>(this.urlWebBase + "/buscarpornome", nome);
+    }
+
+    // public montarDireitoAcesso(): Observable<GrupoResult[]>{
+    //     return this.http.get<GrupoResult[]>(this.urlWebBase + '/montardireitoacesso');
+    // }
+}
